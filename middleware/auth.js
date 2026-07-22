e8f6de17ffaa2cc22b7cc7f5ee537f108fd09e71
Pedro José Moreno Villar <pjmorenovillar@gmail.com>
2026-07-22 12:15:50 +0200

    adapt auth.js to JWT

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,10 +1,12 @@
+import passport from "passport";
+
 export function isAuth(req, res, next) {
-  if (req.isAuthenticated()) {
+  passport.authenticate("jwt", { session: false }, (err, user) => {
+    if (err || !user) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    req.user = user;
     return next();
-  } else {
-    res.status(401).res.json( {
-      title: "Authorization missing",
-      message: `You are not authorized to see this route. Please register or log in.`,
-    });
-  }
+  })(req, res, next);
 }
