6e421d21f5619ef6cbdc58fe350cf025b4cc37b9
Pedro José Moreno Villar <pjmorenovillar@gmail.com>
2026-07-23 16:56:49 +0200

    refactor: update comments

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -6,28 +6,30 @@ import {
   deleteAllUsers,
 } from "../controllers/usersController.js";
 import { getAccessToken } from "../controllers/authController.js";
+import passport from "passport";
 // import { validateRegistration } from "../validators/validateRegistration.js";
 // import { validateLogin } from "../validators/validateLogin.js";
-import passport from "passport";
 
 const usersRouter = Router();
 
-// usersRouter.get("/register", (req, res) => {
-//   res.render("register", { title: "Register user" });
-// });
 
 usersRouter.post("/register", registerUser);
 
 usersRouter.delete("/delete", deleteAllUsers);
 
-// usersRouter.get("/login", (req, res) => {
-//   res.render("login", { title: "Log in" });
-// });
 
 usersRouter.post("/login", loginUser);
 
 usersRouter.post("/logout", logoutUser);
 
 usersRouter.post("/token", getAccessToken);
+
+// usersRouter.get("/register", (req, res) => {
+//   res.render("register", { title: "Register user" });
+// });
+
+// usersRouter.get("/login", (req, res) => {
+//   res.render("login", { title: "Log in" });
+// });
 
 export { usersRouter };
