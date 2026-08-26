ea1016c71feb5d92f1eb8279c8da5f2cc8cbddac
Pedro José Moreno Villar <pjmorenovillar@gmail.com>
2026-08-26 18:54:06 +0200

    test: add mock posts

--- tests/posts.test.js
+++ tests/posts.test.js
@@ -1,7 +1,36 @@
 import supertest from "supertest";
 import app from "../app.js";
+import prisma from "../lib/prisma.js";
 
 const request = supertest(app);
+
+beforeEach(()=> {
+  return prisma.post.deleteMany().then(() => {
+    return prisma.post.createMany({
+    data: [{
+      authorId: 1,
+      title: "Test post",
+      slug: "test-post",
+      content: "post content",
+      isPublished: true
+    },
+    {
+      authorId: 1,
+      title: "Test post 2",
+      slug: "test-post-2",
+      content: "post content",
+      isPublished: true
+    },
+    {
+      authorId: 1,
+      title: "Test post 3",
+      slug: "test-post-3",
+      content: "post content",
+      isPublished: false
+    }],
+  });
+  })
+})
 
 test("GET /api/posts returns published posts", async () => {
   const res = await request.get("/api/posts");
